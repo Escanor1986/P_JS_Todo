1e48f92c774a105c48fab5af030dfb245bfb7863
Escanor1986 <lionel.zovi@gmail.com>
2024-04-27 14:19:40 +0200

    test deploy

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       filename: "index.html",
-      template: path.join(__dirname, "./src/index.html"),
+      template: path.join(__dirname, "./dist/index.html"),
       chunks: ["main"],
     }),
   ],
